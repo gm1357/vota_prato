d2884cc1bed6652d6f7f3d4c8d8489ffbb0f7567
Gabriel Machado <g_machado1@hotmail.com>
2016-03-26 11:10:12 -0300

    create new models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323024643) do
+ActiveRecord::Schema.define(version: 20160326140716) do
+
+  create_table "clientes", force: :cascade do |t|
+    t.string   "nome",       limit: 80
+    t.integer  "idade",      limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pratos", force: :cascade do |t|
+    t.string   "nome",       limit: 80
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
+
+  create_table "receitas", force: :cascade do |t|
+    t.text     "conteudo",   limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "restaurantes", force: :cascade do |t|
     t.string   "nome",          limit: 80
